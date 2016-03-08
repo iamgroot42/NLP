f872a04369fe4a648c9d124e5490c4c8559f41e8
Anshuman <anshuman14021@iiitd.ac.in>
2016-03-08 20:24:16 +0530

    Fixed Facebook scraper. Added Google Translate for python (almost useless here).

---
+++ translate_microsoft.py
@@ -0,0 +1,9 @@
+# from mstranslator import Translator
+
+# CLIENT_ID = 'NLP'
+# CLIENT_SECRET = '7I2PWW9SJDRNj72bIjTqh7xOn7eke+rPNDx94JUKJEA='
+# translator = Translator('NLP', CLIENT_SECRET)
+# x = 'आलू'
+# print(translator.translate(x.decode("utf-8"), lang_from='hi', lang_to='en'))
+
+# Bing's python library no longer supported on pip
